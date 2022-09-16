a59c3c4b27197f757b0f46696db8a483744fa539
Shivam <shiarora4002@gmail.com>
2022-09-16 17:12:42 +0530

    Exceptional Hander Class Added
    ExceptionalHandler Class Added along with controller advice annotation
    to handle all the exceptions.

--- src/main/java/com/gmail/exceptionhandler/ExceptionalHandler.java
+++ src/main/java/com/gmail/exceptionhandler/ExceptionalHandler.java
@@ -1,5 +1,11 @@
 package com.gmail.exceptionhandler;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
 public class ExceptionalHandler {
 
+	public static void main(String[] args) {
+		
+	}
 }
